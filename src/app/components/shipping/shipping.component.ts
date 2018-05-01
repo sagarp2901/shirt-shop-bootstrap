3c84892432fdfcd609764fe78c770c3cec92a2b7
Sagar_Patel <sagarp@dev6.com>
2018-05-01 15:43:07 -0400

    Shipping form basic design implemented

---
+++ src/app/components/shipping/shipping.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { COUNTRIES, REGIONS } from '../../constants/countries-regions.constants';
+@Component({
+  selector: 'app-shipping',
+  templateUrl: './shipping.component.html',
+  styleUrls: ['./shipping.component.css']
+})
+export class ShippingComponent implements OnInit {
+  private states = REGIONS;
+  private countries = COUNTRIES;
+
+  selectedCountry = 'Select Option';
+  selectedState = 'Select';
+
+  contactIntro = 'Welcome to the club, where can we ship your shirts to? You can always provide this information at checkout';
+  constructor() { }
+
+
+
+
+  ngOnInit() {
+  }
+
+  selectCountry(country) {
+    this.selectedCountry = country;
+    this.selectedState = 'Select';
+  }
+
+  selectState(state) {
+    this.selectedState = state;
+  }
+
+}
