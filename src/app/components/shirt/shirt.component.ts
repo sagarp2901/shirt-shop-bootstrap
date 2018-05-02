e6551b384cd4ad55ce59f5f9d4ef48126e5d28f7
Sagar_Patel <sagarp@dev6.com>
2018-05-02 13:04:02 -0400

    Shirts catalog responsive grid created.

---
+++ src/app/components/shirt/shirt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shirt',
+  templateUrl: './shirt.component.html',
+  styleUrls: ['./shirt.component.css']
+})
+export class ShirtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
