73fe9b2cc7a4b0f08cfdce2c3ecba1fe344d74c0
Sagar_Patel <sagarp@dev6.com>
2018-05-03 16:42:30 -0400

    Added bootstrap tabs using bootstrap angular module.

---
+++ src/app/components/models/shirt-list.model.ts
@@ -0,0 +1,10 @@
+export const shirtList = [
+    { id: 1, name: 'Happy Shirt', description: 'Womens Fine Jersey Short Sleeve', price: 14.99, image: '../../../assets/images/WomensShirtDesigns-3.jpg', gender: 'F' },
+    { id: 2, name: '4 Coders', description: 'Mens Fine Jersey Short Sleeve', price: 14.99, image: '../../../assets/images/MensShirtDesigns-2.jpg', gender: 'M' },
+    { id: 3, name: 'Emoji Shirt', description: 'Womens Fine Jersey Short Sleeve', price: 15.99, image: '../../../assets/images/WomensShirtDesigns-2.jpg', gender: 'F' },
+    { id: 4, name: 'Falcon on black', description: 'Mens Fine Jersey Short Sleeve', price: 19.99, image: '../../../assets/images/MensShirtDesigns-3.jpg', gender: 'M' },
+    { id: 5, name: 'Falcon on white', description: 'Mens Fine Jersey Short Sleeve', price: 19.99, image: '../../../assets/images/MensShirtDesigns-4.jpg', gender: 'M' },
+    { id: 6, name: 'Office Space', description: 'Womens Fine Jersey Short Sleeve', price: 14.99, image: '../../../assets/images/WomensShirtDesigns-1.jpg', gender: 'F' },
+    { id: 7, name: 'Smile', description: 'Mens Fine Jersey Short Sleeve', price: 15.99, image: '../../../assets/images/MensShirtDesigns-1.jpg', gender: 'M' },
+    { id: 8, name: 'Dabbing Skeleton', description: 'Mens Fine Jersey Short Sleeve', price: 19.99, image: '../../../assets/images/MensShirtDesigns-5.jpg', gender: 'M' }
+];
